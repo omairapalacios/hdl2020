5b4c18324e35dfd6ab61a510d6ae760d0bee8fde
omairapalacios <cpalaciosanchez@hotmail.com>
2020-08-22 10:51:39 -0500

    Update styles'

--- src/app/interfaces/mentor.ts
+++ src/app/interfaces/mentor.ts
@@ -4,6 +4,5 @@ export interface Mentor {
   profesion: string;
   bio: string;
   img: string;
-  grupo: string;
   id?: number;
 }
--- src/assets/data/mentores.ts
+++ src/assets/data/mentores.ts
@@ -6,7 +6,6 @@ export const DATA = [
     profesion: 'Ingeniera Electrónica',
     bio: 'El cerebro de la operación. Un hombre que nadie asociaría a priori a ningún plan criminal. Dialogante, de fina ironía y autodidacta, es la antítesis de cualquiera de los individuos que integran su banda. De inteligencia superior, ha planeado durante años hasta el mínimo detalle de la operación. Parece saberlo todo, anticiparse a todo desde su puesto de observación en el exterior. No obstante, no contaba con establecer un vínculo tan fuerte con la negociadora policial, una mujer que le va a marcar.',
     img: 'assets/img/mentora1.jpeg',
-    grupo: 'atracadores'
   },
   {
     alias: 'Alexandra Fernandez',
@@ -14,7 +13,6 @@ export const DATA = [
     profesion: 'Cientifica de Datos',
     bio: 'Una infancia programando y hackeando. Una adolescencia con la mirada en la Red. Esa capacidad y esos conocimientos le hicieron jefe técnico de una empresa de seguridad siendo aún muy joven. En lo personal, todo ese saber no vino acompañado de un grado de madurez equiparable. Así, pronto se vio metido en hackeos y roturas de sistemas de seguridad que poco tenían de juego. Terminó en búsqueda y captura por la Interpol, situación de la que le rescató El Profesor. Es el maestro tecnológico del grupo.',
     img: 'assets/img/mentora2.jpeg',
-    grupo: 'atracadores'
   },
   {
     alias: 'Natali Tauma',
@@ -22,7 +20,6 @@ export const DATA = [
     profesion: 'Ingeniera Civil',
     bio: 'Es el jefe al mando en el interior de la Fábrica de Moneda. Ladrón de guante blanco especializado en el robo de joyas. Ingresó en el mundo del crimen para mantener el altísimo nivel de vida al que estaba acostumbrado desde la cuna. Es un hombre que sabe tener modales finos, que se cree superior al resto de la banda, a quienes desprecia íntimamente. Pero también sabe ser brutal y directo cuando conviene. Su talón de Aquiles son las mujeres, y esta será la circunstancia que ponga en riesgo su liderazgo.',
     img: 'assets/img/mentora3.jpeg',
-    grupo: 'atracadores'
   },
   {
     alias: 'Sonia Alvarez',
@@ -30,7 +27,6 @@ export const DATA = [
     profesion: 'Ingeniera Mecánica',
     bio: 'Hijo de Moscú. Macarra de extrarradio, un poco corto de miras e impulsivo. Alternó su juventud entre los botellones del parking del Caprabo y las barracas de la feria. Coqueteó con todas las drogas posibles y quizás por tal abuso, hay una conexión en su cerebro que salta a “modo irascible” sin motivo alguno. Genera tensión por donde pasa, y es mejor no tratar en exceso con él. Su padre es su único anclaje con la humanidad y los sentimientos. Haría cualquier cosa por él.',
     img: 'assets/img/mentora4.jpeg',
-    grupo: 'atracadores'
   },
   {
     alias: 'Mariela Sulca',
@@ -38,7 +34,6 @@ export const DATA = [
     profesion: 'Ingenieria Informatica',
     bio: 'Una joven con una dura infancia que tuvo que ganarse la vida realizando falsificaciones. Embarazada y abandonada por un novio que tuvo en la adolescencia, no vio otra opción que el delito para salir adelante. Su faceta "creativa" le vino muy bien, y pronto era una cotizada falsificadora de todo tipo de cosas, siempre que tuvieran valor material. Tras pasar por prisión, perdió la custodia del pequeño. Con este golpe aspira a recuperarlo y retirarse con él a un lugar tranquilo.',
     img: 'assets/img/mentora5.jpeg',
-    grupo: 'atracadores'
   },
   {
     alias: 'Sara Benitez',
@@ -46,7 +41,6 @@ export const DATA = [
     profesion: 'Ingeniera en Ciberseguridad',
     bio: 'Serbio. Es la fuerza bruta del grupo. Poco se sabe de él, más allá del evidente pasado de violencia y dureza que ha vivido; y la poca inclinación al diálogo y a hacerse preguntas ante las dificultades que muestra en el presente. Es el soldado de choque perfecto en el plan de El Profesor.',
     img: 'assets/img/mentora6.jpeg',
-    grupo: 'atracadores'
   },
   {
     alias: 'Mariela Sulca',
@@ -54,7 +48,6 @@ export const DATA = [
     profesion: 'Ingenieria Informatica',
     bio: 'Una joven con una dura infancia que tuvo que ganarse la vida realizando falsificaciones. Embarazada y abandonada por un novio que tuvo en la adolescencia, no vio otra opción que el delito para salir adelante. Su faceta "creativa" le vino muy bien, y pronto era una cotizada falsificadora de todo tipo de cosas, siempre que tuvieran valor material. Tras pasar por prisión, perdió la custodia del pequeño. Con este golpe aspira a recuperarlo y retirarse con él a un lugar tranquilo.',
     img: 'assets/img/mentora7.jpeg',
-    grupo: 'atracadores'
   },
   {
     alias: 'Sara Benitez',
@@ -62,6 +55,5 @@ export const DATA = [
     profesion: 'Darko Peric',
     bio: 'Serbio. Es la fuerza bruta del grupo. Poco se sabe de él, más allá del evidente pasado de violencia y dureza que ha vivido; y la poca inclinación al diálogo y a hacerse preguntas ante las dificultades que muestra en el presente. Es el soldado de choque perfecto en el plan de El Profesor.',
     img: 'assets/img/mentora8.jpeg',
-    grupo: 'atracadores'
   },
 ];
