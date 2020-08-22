95974720c7dd1b987776fb6c40da0bf40ffec82e
PaolaS2992 <huaripayta29.92@gmail.com>
2020-08-22 09:41:23 -0500

    Add component card-home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CarruselComponent } from './components/carrusel/carrusel.component';
 import { MentoresComponent } from './components/mentores/mentores.component';
 import { MentorCardComponent } from './components/mentor-card/mentor-card.component';
 import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { CardHomeComponent } from './components/card-home/card-home.component';
 
 
 @NgModule({
@@ -20,6 +21,7 @@ import { AboutmeComponent } from './components/aboutme/aboutme.component';
     MentoresComponent,
     MentorCardComponent,
     AboutmeComponent,
+    CardHomeComponent,
   ],
   imports: [
     BrowserModule,
